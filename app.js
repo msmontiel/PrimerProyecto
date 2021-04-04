6606aa8ad514a31c2724ea1681b08b15e675a25d
msmontiel <80135438+msmontiel@users.noreply.github.com>
2021-04-04 19:05:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 "use strict"
+var http = require('http');
+var fs = require('fs')
 
-alert("Editado");
+http.createServer(function (peticion, respuesta) {
+    respuesta.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    switch (peticion.url) {
+        case '/':
+            plantilla = 'index.html'
+            break;
+        case '/nodejs':
+            plantilla = 'sobreNode.html';
+            break;
+        default:
+            plantilla = "404.html"
+            break;
+    }
+    fs.readFile("./plantillas/" + plantilla, function (error, datos) {
+
+        respuesta.write(datos);
+        respuesta.end()
+    });
+
+}).listen(3000, 'localhost');
+
+console.log('El servidor se muestra en http://localhost:3000');
